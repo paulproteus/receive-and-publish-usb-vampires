e091755c2e83e7ca746f0b7aad382496840b5699
Asheesh Laroia <asheesh@asheesh.org>
2013-01-30 14:03:57 +1100

    Make a semi useful landing page

--- vampire_list/__init__.py
+++ vampire_list/__init__.py
@@ -4,5 +4,9 @@ import bottle
 def submit():
     return "fail"
 
+@bottle.route('/')
+def home():
+    return 'Hello! Read <a href="https://github.com/paulproteus/receive-and-publish-usb-vampires/blob/master/README">README</a>.'
+
 if __name__ == '__main__':
     bottle.run(host='localhost', port=8080)
