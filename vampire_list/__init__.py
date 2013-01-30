75d77d03b64b01ed005d4c080e94f2e78a88cd31
Asheesh Laroia <asheesh@asheesh.org>
2013-01-30 14:43:59 +1100

    Be able to list submissions

--- vampire_list/__init__.py
+++ vampire_list/__init__.py
@@ -1,26 +1,58 @@
 import bottle
 import os
+import glob
+
+MAGIC_STRING = "Save us from vampires"
+
+def find_next_available_data_path(i_start=0):
+    i = i_start
+    while True:
+        try:
+            os.mkdir("data/%d" % (i,))
+            # Yay, it worked! Return.
+            return i
+        except OSError, e:
+            if e.errno == 17:
+                # Already exists. OK, increment, and loop.
+                i += 1
+            else:
+                raise # What the heck is going on?
 
 @bottle.post('/')
 def submit():
-    print bottle.request.forms.get('payload')
-    return "Saved as number ??"
+    s = bottle.request.forms.get('payload')
+    # Check for our magic string
+    if MAGIC_STRING not in s:
+        return bottle.abort(403, "Missing magic string")
+
+    num = find_next_available_data_path()
+    with open("data/%d/txt" % (num,), 'w') as fd:
+        fd.write(s)
+    return "Saved as number %d" % (num,)
 
 @bottle.route('/read/')
 def read_index():
-    return "List them here"
+    things = sorted([int(x.split('/')[1])
+                     for x in glob.glob("data/*/txt")])
+    s = "<ul>"
+    for thing in things:
+        s += '<li><a href="/read/%d">%d</a></li>' % (
+            thing, thing)
+    s += "</ul>"
+    return s
 
 @bottle.route('/read/:number')
 def read(number=None):
     n = int(number)
-    path = "data/%d.txt" % (n,)
+    path = "data/%d/txt" % (n,)
     if not os.path.exists(path):
         return bottle.abort(404)
+    bottle.response.set_header('Content-Type', 'text/plain')
     return open(path).read()
 
 @bottle.route('/')
 def home():
-    return 'Hello! Read <a href="https://github.com/paulproteus/receive-and-publish-usb-vampires/blob/master/README">README</a>.'
+    return 'Hello!<p>Read <a href="https://github.com/paulproteus/receive-and-publish-usb-vampires/blob/master/README">README</a>.</p><p>Or look at the <a href="/read/">list of posts</a>.</p>'
 
 if __name__ == '__main__':
     bottle.run(host='localhost', port=8080)
